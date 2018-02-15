cb45191344cf947f25145246b696bb361e88349b
phytertek <ryan.phytertek@gmail.com>
2018-02-15 00:41:31 -0500

    updated index

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const enmapi = require('enmapi');
 
 enmapi.server.setConfig({
-  Level: process.env.NODE_ENV || 'development',
-  Name: process.env.NAME || 'Eviluators Admin Api',
-  Host: process.env.HOST || 'http://localhost',
-  Port: process.env.PORT || 3333,
-  DatabaseName: process.env.DBNAME || 'Eviluators Admin DB',
-  DatabaseURI:
-    process.env.DB_URI ||
-    'mongodb://gJKS4LFEnsSFDg2lx2JQ28XwvSxgi62H:wwDfyiCMQDHCc1b0KGCwGflRCR59Eiyr@ds235778.mlab.com:35778/eviluators-admin-api'
+  Level: process.env.NODE_ENV,
+  Name: process.env.NAME,
+  Host: process.env.HOST,
+  Port: process.env.PORT,
+  DatabaseName: process.env.DBNAME,
+  DatabaseURI: process.env.DB_URI
 });
 enmapi.server.start();
-
-//process.env.JWT_SECRET;
